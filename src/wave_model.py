a95f174adb70c15182d21e54e38d1aa9b83326b2
Gina Santana <endernova@Ginas-MacBook-Pro.local>
2020-08-26 17:56:20 -0600

    update wave file

--- src/wave_model.py
+++ src/wave_model.py
@@ -5,51 +5,49 @@ from skimage.transform import resize, rotate
 import PIL
 import os
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img, smart_resize
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
 
 
 def cnn_model():
 
     model = Sequential()
-    model.add(Conv2D(32, (5, 5), input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dense(100)) # added
-    model.add(Conv2D(32, (5, 5)))
+    model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dense(100)) # added
-    model.add(Conv2D(32, (5, 5)))
+    model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))  
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dense(100)) # added
-    model.add(Conv2D(32, (5, 5)))  
+    model.add(Conv2D(32, (3, 3)))  
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dense(100)) # added
-    model.add(Conv2D(64, (5, 5))) 
+    model.add(Conv2D(64, (3, 3))) 
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten()) 
-    model.add(Dense(100)) # was 500
+    model.add(Dense(500)) # was 500
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1)) # single output neuron (output ranged from 0-1; binary class)
     model.add(Activation('sigmoid'))
     model.compile(loss='binary_crossentropy',
-                optimizer='adam', 
+                optimizer=Adam(), 
                 metrics=['accuracy'])
     return model
 
 
+
 if __name__=='__main__':
-    img_width, img_height = 256, 256
+    img_width, img_height = 128, 128 # CHANGE TO 512 X ?
     train_data_dir = 'data/wave/training'
     validation_data_dir = 'data/wave/testing'
-    epochs = 275
+    epochs = 300
     batch_size = 24
 
     if K.image_data_format() == 'channels_first':
@@ -59,20 +57,21 @@ if __name__=='__main__':
 
     model = cnn_model() 
 
-    spiral_train_datagen = ImageDataGenerator(
-        zoom_range=0.1,
+    wave_train_datagen = ImageDataGenerator(
+        zoom_range=0.2,
         height_shift_range=0.1,
         rescale=1./255,
         shear_range=0.2,
-        horizontal_flip=True) #was false
+        horizontal_flip=True,
+        ) 
 
     # this is the augmentation configuration for testing:
-    spiral_test_datagen = ImageDataGenerator(rescale=1./255)
+    wave_test_datagen = ImageDataGenerator(rescale=1./255)
 
     # this is a generator that will read pictures found in
     # subfolers of 'data/train', and indefinitely generate
     # batches of augmented image data
-    train_generator = spiral_train_datagen.flow_from_directory(
+    train_generator = wave_train_datagen.flow_from_directory(
             train_data_dir, 
             target_size=(img_width, img_height),
             batch_size=24,
@@ -81,7 +80,7 @@ if __name__=='__main__':
             ) 
 
     # this is a similar generator, for validation data
-    validation_generator = spiral_test_datagen.flow_from_directory(
+    validation_generator = wave_test_datagen.flow_from_directory(
             validation_data_dir,
             target_size=(img_width, img_height),
             batch_size=30,
