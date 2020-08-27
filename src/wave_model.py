5860428f4c8b8e156cabc15fc0b9efd259eec372
Gina Santana <endernova@Ginas-MacBook-Pro.local>
2020-08-26 19:26:39 -0600

    Adding final model version of wave file with evaluation plots

--- src/wave_model.py
+++ src/wave_model.py
@@ -19,9 +19,6 @@ def cnn_model():
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))  
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(32, (3, 3)))  
@@ -31,7 +28,7 @@ def cnn_model():
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten()) 
-    model.add(Dense(500)) # was 500
+    model.add(Dense(500)) 
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1)) # single output neuron (output ranged from 0-1; binary class)
@@ -44,10 +41,10 @@ def cnn_model():
 
 
 if __name__=='__main__':
-    img_width, img_height = 128, 128 # CHANGE TO 512 X ?
+    img_width, img_height = 128, 128 
     train_data_dir = 'data/wave/training'
     validation_data_dir = 'data/wave/testing'
-    epochs = 300
+    epochs = 400
     batch_size = 24
 
     if K.image_data_format() == 'channels_first':
@@ -107,6 +104,6 @@ if __name__=='__main__':
     plt.ylim([0,1])
     plt.legend(loc='lower right')
     plt.show()
-    plt.savefig('275waveV3.png')
+    plt.savefig('1000.png')
 
     model.save_weights('try.h5') 
